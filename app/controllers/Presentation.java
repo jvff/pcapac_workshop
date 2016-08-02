e749ba08077c8d7fb5aa178d0f3061a9a5a39554
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-08-02 11:49:23 +0000

    Create next button
    - Create thanks slide
    - For now the button simply goes to the thanks slide

--- app/controllers/Presentation.java
+++ app/controllers/Presentation.java
@@ -4,9 +4,14 @@ import play.mvc.Controller;
 import play.mvc.Result;
 
 import views.html.Presentation.start;
+import views.html.Presentation.slides.thanks;
 
 public class Presentation extends Controller {
     public static Result start() {
         return ok(start.render());
+    }
+
+    public static Result slide(Integer number) {
+        return ok(thanks.render());
     }
 }
