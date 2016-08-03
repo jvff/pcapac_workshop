cc730b7d5f91e60601a93d2dc939d78e8309fc7b
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-08-03 14:47:10 +0000

    Refactor slide rendering to use a slide list
    - Create a SlideHandler class that renders a slide (using reflection)
    - Create a static array of SlideHandlers to represent all the available
      slides
    - Update the slide request handler to use the respective SlideHandler

--- app/controllers/Presentation.java
+++ app/controllers/Presentation.java
@@ -1,21 +1,54 @@
 package controllers;
 
+import java.lang.reflect.Method;
+
 import play.mvc.Controller;
 import play.mvc.Result;
+import play.twirl.api.Html;
 
 import views.html.Presentation.start;
 import views.html.Presentation.slides.*;
 
 public class Presentation extends Controller {
+    private static class SlideHandler {
+        private Method renderMethod;
+
+        SlideHandler(Class slideClass) {
+            try {
+                renderMethod = slideClass.getDeclaredMethod("render");
+            } catch (NoSuchMethodException exception) {
+                renderMethod = null;
+            }
+        }
+
+        Result render() {
+            if (renderMethod == null)
+                return internalServerError();
+
+            try {
+                return ok((Html)renderMethod.invoke(null));
+            } catch (Exception exception) {
+                return internalServerError();
+            }
+        }
+    }
+
+    private static SlideHandler[] slides;
+
+    static {
+        slides = new SlideHandler[] {
+            new SlideHandler(title.class),
+            new SlideHandler(thanks.class)
+        };
+    }
+
     public static Result start() {
         return ok(start.render());
     }
 
     public static Result slide(Integer number) {
-        if (number == 0)
-            return ok(title.render());
-        else if (number == 1)
-            return ok(thanks.render());
+        if (number >= 0 && number < slides.length)
+            return slides[number].render();
         else
             return notFound();
     }
