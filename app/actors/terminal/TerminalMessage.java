def397763ea3207d12787b406c63c4677168b0e0
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-10-21 17:13:39 +0000

    Refactor to create TerminalMessage class

---
+++ app/actors/terminal/TerminalMessage.java
@@ -0,0 +1,37 @@
+package actors.terminal;
+
+import java.io.IOException;
+import java.io.Writer;
+
+public static abstract class TerminalMessage {
+    public abstract void writeTo(Writer out) throws IOException;
+
+    protected void writeInt(int value, Writer out) throws IOException {
+        writeAlgorisms(value, out);
+        out.write(';');
+    }
+
+    private void writeAlgorisms(int value, Writer out) throws IOException {
+        while (value > 0) {
+            int algorism = value % 64;
+
+            writeAlgorism(algorism, out);
+
+            value /= 64;
+        }
+    }
+
+    private void writeAlgorism(int algorism, Writer out)
+            throws IOException {
+        if (algorism < 26)
+            out.write(algorism + 'A');
+        else if (algorism < 52)
+            out.write(algorism - 26 + 'a');
+        else if (algorism < 62)
+            out.write(algorism - 52 + '0');
+        else if (algorism == 62)
+            out.write('+');
+        else if (algorism == 63)
+            out.write('/');
+    }
+}
