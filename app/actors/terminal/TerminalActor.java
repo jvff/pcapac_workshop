dbb5d4a326087e00f2cb1cf36a78ad9acd906502
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-10-21 18:30:37 +0000

    Refactor to create TerminalContainer
    - Isolate Docker code from Terminal actor
    - Create helper methods to forward operations to the container listener
    - Change connection code to avoid initial delay and to retry 5 times in
      0.5 second intervals
    - Create a helper method to register a listener for container data
      - Method starts the listener thread and forwards all received data to
        the listener

--- app/actors/terminal/TerminalActor.java
+++ app/actors/terminal/TerminalActor.java
@@ -1,16 +1,10 @@
 package actors.terminal;
 
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Reader;
-import java.io.Writer;
-import java.net.Socket;
 
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.PoisonPill;
 import akka.actor.Props;
 import akka.actor.UntypedActor;
 
@@ -18,20 +12,11 @@ import play.libs.Akka;
 import play.libs.F;
 import play.Logger;
 
-import com.github.dockerjava.api.command.CreateContainerResponse;
-import com.github.dockerjava.api.DockerClient;
-import com.github.dockerjava.core.DefaultDockerClientConfig;
-import com.github.dockerjava.core.DockerClientBuilder;
-import com.github.dockerjava.core.DockerClientConfig;
-
 import actors.terminal.TerminalManagerActor.NewTerminalMessage;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
-import static com.github.dockerjava.core.RemoteApiVersion.VERSION_1_23;
-
 public class TerminalActor extends UntypedActor {
-    private static final int CONTAINER_PORT = 15100;
     private static final char CMD_KEY = 'k';
     private static final char CMD_RESIZE = 'r';
     private static final char CMD_UPLOAD = 'u';
@@ -45,120 +30,33 @@ public class TerminalActor extends UntypedActor {
     }
 
     private final ActorRef out;
-
-    private String containerAddress;
-    private Socket containerSocket;
-    private Writer containerWriter;
-    private Reader containerReader;
-
-    private Thread listenerThread = new Thread(new Runnable() {
-        @Override
-        public void run() {
-            try {
-                runListenerLoop();
-            } catch (IOException exception) {
-                exception.printStackTrace();
-            }
-        }
-    });
+    private TerminalContainer container;
 
     public TerminalActor(ActorRef out, String sessionId) {
         this.out = out;
 
-        startContainer();
-        notifyInstantiationToTerminalManager(sessionId);
+        if (tryToStartContainer())
+            notifyInstantiationToTerminalManager(sessionId);
     }
 
-    @Override
-    public void onReceive(Object message) {
+    private boolean tryToStartContainer() {
         try {
-            unsafelyHandleMessage(message);
-        } catch (IOException exception) {
-            Logger.error("Failed to handle message: " + message);
+            startContainer();
+
+            return true;
+        } catch (IOException cause) {
+            Logger.error("Failed to start container", cause);
+
+            self().tell(PoisonPill.getInstance(), self());
+
+            return false;
         }
     }
 
-    private void unsafelyHandleMessage(Object message) throws IOException {
-        if (message instanceof String)
-            sendDataToContainer((String)message);
-        else if (message instanceof ResizeTerminalMessage)
-            resizeContainerTerminal((ResizeTerminalMessage)message);
-        else if (message instanceof UploadFileMessage)
-            uploadFileToContainer((UploadFileMessage)message);
-    }
+    private void startContainer() throws IOException {
+        container = new TerminalContainer();
 
-    private void startContainer() {
-        createContainer();
-        connectToContainer();
-        listenerThread.start();
-    }
-
-    private void createContainer() {
-        DockerClientConfig dockerConfig = DefaultDockerClientConfig
-            .createDefaultConfigBuilder()
-            .withDockerHost("unix:///var/run/docker.sock")
-            .withApiVersion(VERSION_1_23)
-            .build();
-
-        DockerClient docker = DockerClientBuilder.getInstance(dockerConfig)
-            .build();
-
-        String containerId = docker
-            .createContainerCmd("janitovff/pcapac_git_shell")
-            .exec()
-            .getId();
-
-        Logger.debug("Created container: " + containerId);
-
-        docker.startContainerCmd(containerId).exec();
-
-        Logger.debug("Started container: " + containerId);
-
-        containerAddress = docker.inspectContainerCmd(containerId)
-            .exec()
-            .getNetworkSettings()
-            .getIpAddress();
-
-        Logger.debug("IP address of container " + containerId + " is "
-                + containerAddress);
-    }
-
-    private void connectToContainer() {
-        try {
-            Logger.debug("Waiting to connect to container at "
-                    + containerAddress + ":" + CONTAINER_PORT);
-
-            Thread.sleep(2000);
-
-            Logger.debug("Connecting to container at " + containerAddress + ":"
-                    + CONTAINER_PORT);
-
-            containerSocket = new Socket(containerAddress, CONTAINER_PORT);
-
-            Logger.debug("Connected to container at " + containerAddress + ":"
-                    + CONTAINER_PORT);
-
-            InputStream socketInput = containerSocket.getInputStream();
-            OutputStream socketOut = containerSocket.getOutputStream();
-
-            containerReader = new InputStreamReader(socketInput, UTF_8);
-            containerWriter = new OutputStreamWriter(socketOut, UTF_8);
-        } catch (Exception cause) {
-            Logger.error("Failed to connect to container at " + containerAddress
-                    + ":" + CONTAINER_PORT, cause);
-        }
-    }
-
-    private void runListenerLoop() throws IOException {
-        final int END_OF_STREAM_INDICATOR = -1;
-
-        int character = containerReader.read();
-
-        while (character != END_OF_STREAM_INDICATOR) {
-            sendCharacter((char)character);
-
-            character = containerReader.read();
-        }
+        container.registerListener(character -> sendCharacter(character));
     }
 
     private void sendCharacter(char character) {
@@ -167,30 +65,29 @@ public class TerminalActor extends UntypedActor {
         out.tell(message, self());
     }
 
-    private void sendDataToContainer(String dataMessage) throws IOException {
-        containerWriter.write(CMD_KEY);
-        containerWriter.write(dataMessage);
-        containerWriter.flush();
-    }
-
-    private void resizeContainerTerminal(ResizeTerminalMessage message)
-            throws IOException {
-        containerWriter.write(CMD_RESIZE);
-
-        message.writeTo(containerWriter);
-    }
-
-    private void uploadFileToContainer(UploadFileMessage message)
-            throws IOException {
-        containerWriter.write(CMD_UPLOAD);
-
-        message.writeTo(containerWriter);
-    }
-
     private void notifyInstantiationToTerminalManager(String sessionId) {
         ActorSystem system = Akka.system();
         ActorRef manager = system.actorFor(TerminalManagerActor.URL);
 
         manager.tell(new NewTerminalMessage(sessionId), self());
+    }
+
+    @Override
+    public void onReceive(Object message) {
+        try {
+            if (container != null)
+                unsafelyHandleMessage(message);
+        } catch (IOException exception) {
+            Logger.error("Failed to handle message: " + message);
+        }
+    }
+
+    private void unsafelyHandleMessage(Object message) throws IOException {
+        if (message instanceof String)
+            container.sendData(CMD_KEY, (String)message);
+        else if (message instanceof ResizeTerminalMessage)
+            container.sendOperation(CMD_RESIZE, (TerminalMessage)message);
+        else if (message instanceof UploadFileMessage)
+            container.sendOperation(CMD_UPLOAD, (TerminalMessage)message);
     }
 }
---
+++ app/actors/terminal/TerminalContainer.java
@@ -0,0 +1,172 @@
+package actors.terminal;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
+import java.net.Socket;
+import java.util.function.Consumer;
+
+import play.Logger;
+
+import com.github.dockerjava.api.command.CreateContainerResponse;
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.dockerjava.core.DockerClientBuilder;
+import com.github.dockerjava.core.DockerClientConfig;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import static com.github.dockerjava.core.RemoteApiVersion.VERSION_1_23;
+
+public class TerminalContainer {
+    private static final int CONTAINER_PORT = 15100;
+
+    private DockerClient docker;
+    private String ipAddress;
+    private String id;
+
+    private Socket socket;
+    private Writer writer;
+    private Reader reader;
+
+    private Consumer<Character> listener;
+
+    public TerminalContainer() throws IOException {
+        createDockerClient();
+        createContainer();
+        startContainer();
+        connectToContainer();
+    }
+
+    private void createDockerClient() {
+        DockerClientConfig dockerConfig = DefaultDockerClientConfig
+            .createDefaultConfigBuilder()
+            .withDockerHost("unix:///var/run/docker.sock")
+            .withApiVersion(VERSION_1_23)
+            .build();
+
+        docker = DockerClientBuilder.getInstance(dockerConfig).build();
+    }
+
+    private void createContainer() {
+        id = docker.createContainerCmd("janitovff/pcapac_git_shell")
+            .exec()
+            .getId();
+
+        Logger.debug("Created container: " + id);
+    }
+
+    private void startContainer() {
+        docker.startContainerCmd(id).exec();
+
+        Logger.debug("Started container: " + id);
+    }
+
+    private void connectToContainer() throws IOException {
+        getIpAddress();
+        openConnection();
+        configureStreams();
+    }
+
+    private void getIpAddress() {
+        ipAddress = docker.inspectContainerCmd(id)
+            .exec()
+            .getNetworkSettings()
+            .getIpAddress();
+
+        Logger.debug("IP address of container " + id + " is " + ipAddress);
+    }
+
+    private void openConnection() {
+        boolean connected = false;
+        int connectionAttempt = 0;
+        int maxAttempts = 5;
+
+        while (!connected && connectionAttempt < maxAttempts) {
+            try {
+                connected = tryToOpenConnection();
+            } catch (IOException cause) {
+                Logger.error("Failed to connect to container at " + ipAddress
+                        + ":" + CONTAINER_PORT, cause);
+
+                connectionAttempt += 1;
+
+                delay(500);
+            }
+        }
+    }
+
+    private void delay(long miliseconds) {
+        try {
+            Thread.sleep(miliseconds);
+        } catch (InterruptedException cause) {
+            Logger.warn("Interrupted during connection retry delay", cause);
+        }
+    }
+
+    private boolean tryToOpenConnection() throws IOException {
+        Logger.debug("Connecting to container at " + ipAddress + ":"
+                + CONTAINER_PORT);
+
+        socket = new Socket(ipAddress, CONTAINER_PORT);
+
+        Logger.debug("Connected to container at " + ipAddress + ":"
+                + CONTAINER_PORT);
+
+        return true;
+    }
+
+    private void configureStreams() throws IOException {
+        InputStream socketInput = socket.getInputStream();
+        OutputStream socketOut = socket.getOutputStream();
+
+        reader = new InputStreamReader(socketInput, UTF_8);
+        writer = new OutputStreamWriter(socketOut, UTF_8);
+    }
+
+    public void sendData(char operation, String data) throws IOException {
+        writer.write(operation);
+        writer.write(data);
+        writer.flush();
+    }
+
+    public void sendOperation(char operation, TerminalMessage message)
+            throws IOException {
+        writer.write(operation);
+        message.writeTo(writer);
+        writer.flush();
+    }
+
+    public void registerListener(Consumer<Character> listener) {
+        this.listener = listener;
+
+        listenerThread.start();
+    }
+
+    private Thread listenerThread = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            try {
+                runListenerLoop();
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+        }
+    });
+
+    private void runListenerLoop() throws IOException {
+        final int END_OF_STREAM_INDICATOR = -1;
+
+        int character = reader.read();
+
+        while (character != END_OF_STREAM_INDICATOR) {
+            listener.accept((char)character);
+
+            character = reader.read();
+        }
+    }
+}
